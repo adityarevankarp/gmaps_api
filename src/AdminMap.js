dcde31158708f708fbcd7cc052bd10e6c1651968
Aditya Revankar <adityarevankar03@gmail.com>
2023-09-13 15:47:30 +0530

    f

---
+++ src/AdminMap.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function AdminMap(props) {
+    console.log(props)
+  return (
+    <div>
+      
+    </div>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import {GoogleMap, Marker, useLoadScript} from "@react-google-maps/api";
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import './App.css';
 
 import { REACT_APP_GOOGLE_MAPS_KEY } from './key'
-import { useEffect, useMemo ,useState} from "react";
+import { useEffect, useMemo, useState } from "react";
+import AdminMap from "./AdminMap.js";
 function App() {
-  
-  const {isLoaded}= useLoadScript({
-    googleMapsApiKey:REACT_APP_GOOGLE_MAPS_KEY,
-    libraries:[],
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: REACT_APP_GOOGLE_MAPS_KEY,
+    libraries: [],
   });
 
-  if(!isLoaded) return <div>Loading...</div>
+  if (!isLoaded) return <div>Loading...</div>
 
   return (
     <div>
@@ -18,68 +19,88 @@ function App() {
     </div>
   );
 }
-function Map(){
-  const [latitude,setLatitude] = useState('');
-  const [longitude,setLongitude] = useState('');
-  useEffect(()=>{
-    navigator.geolocation.getCurrentPosition((position)=>{
-      console.log(position.coords.latitude,position.coords.longitude)
-      setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);
-    })
-  },[])
-  const center = { lat: latitude, lng: longitude };
+function Map() {
+  // const [curLat, setCurLat] = useState('');
+  // const [curLng, setCurLng] = useState('');
+  // useEffect(() => {
+  //   navigator.geolocation.getCurrentPosition((position) => {
+      
+  //     setCurLat(position.coords.latitude);
+  //     setCurLng(position.coords.longitude);
+  //     console.log(curLat,curLng);
+  //   })
+  // }, []) 
+  const [currentLocation, setCurrentLocation] = useState(null);
+  useEffect(() => {
+    
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const userLocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        setCurrentLocation(userLocation);
+        setMarkers([userLocation]);
+        console.log(userLocation)
+      },
+      (error) => {
+        console.error("Error getting user's location:", error);
+      }
+    );
+  }, []);
   
-  const [markers, setMarkers] = useState([]);
+
+  const [markers, setMarkers] = useState([
+   
+  ]);
 
   const handleMapClick = (mapsMouseEvent) => {
-    if(markers.length == 0){
+    if (markers.length === 1) {
       const newMarker = {
         lat: mapsMouseEvent.latLng.lat(),
-        lng: mapsMouseEvent.latLng.lng(),};
-        console.log(newMarker.lat,newMarker.lng);
-        setMarkers([...markers, newMarker]);
-        console.log(markers.length)
-      }
-      else{
-        return null
-      }
+        lng: mapsMouseEvent.latLng.lng(),
+      };
+      console.log(newMarker.lat, newMarker.lng);
+      setMarkers([...markers, newMarker]);
+      console.log(markers.length)
     }
-    
+    else {
+      return null
+    }
+  }
+  const center = currentLocation || { lat: 12.9716, lng: 77.5946 };
 
-    const handleMarkerDragEnd = (index, event) => {
-      // Log the marker's position after dragging
-      console.log(event.latLng.lat(), event.latLng.lng());
-    };
-  
+  const handleMarkerDragEnd = (index, event) => {
 
-    
+    console.log(event.latLng.lat(), event.latLng.lng());
+  };
   return (
+    <div>
+      <AdminMap userLocation={currentLocation}/>
+    
 
-    
-  <GoogleMap 
-    zoom={15} 
-    center={center} 
-    mapContainerClassName="map-container" 
-    
-    
-     
-    onClick={handleMapClick}
+    {/* <GoogleMap
+      zoom={15}
+      center={center}
+      mapContainerClassName="map-container"
+      onClick={handleMapClick}
     >
-      <Marker position={center}/>
+      
+
+
       {markers.map((marker, index) => (
         <Marker
           key={index}
           position={marker}
-          draggable={true} 
+          draggable={true}
           onDragEnd={(event) => handleMarkerDragEnd(index, event)}
-          
-        />
-        
-      ))}
-    
-  </GoogleMap>
 
+        />
+
+      ))}
+
+    </GoogleMap> */}
+    </div>
   );
 
 }
